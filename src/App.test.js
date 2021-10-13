74431c6b9a17a394260d0795ee62406ac6ad1a0b
Alexandr <yaborodinov@gmail.com>
2021-10-13 10:22:06 +0300

    created project, made render tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { shallow } from 'enzyme';
+
+describe('Counter testing', () => {
+  test('render the title of counter', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('h1').text()).toContain('some info');
+  });
+
+  test('render a button with text of `increment`', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('#increment-btn').text()).toBe('Increment')
+  })
+})
